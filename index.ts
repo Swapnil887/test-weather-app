9ad86c2c3b77d89d1ec7373a0a4e9fb810921c86
Swapnil Solanki <78586974+Swapnil887@users.noreply.github.com>
2026-05-15 16:27:40 +0530

    completing weather app

--- index.ts
+++ index.ts
@@ -57,11 +57,11 @@ const startServer = async () => {
       },
     );
 
-    app.listen(3000, () => {
-      //console.log("Server is running on port 3000");
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port 3000");
     });
   } catch (error) {
-    //console.error("Failed to start server", error);
+    console.error("Failed to start server", error);
     process.exit(1);
   }
 };
